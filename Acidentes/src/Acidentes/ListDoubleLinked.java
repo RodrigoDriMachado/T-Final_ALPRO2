4e4c6e1510390553cf09fd3cea2e6891e3d7f873
RodrigoDriMachado <rodrigoodm46@gmail.com>
2014-11-04 19:31:41 -0200

    Correção da classe ListDoubleLinked

--- Acidentes/src/Acidentes/ListDoubleLinked.java
+++ Acidentes/src/Acidentes/ListDoubleLinked.java
@@ -5,13 +5,20 @@
  */
 package Acidentes;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  *
  * @author RODRIGO
  */
 public class ListDoubleLinked<E> implements ListTAD<E> {
 
-        private class Node<T> {
+    private Node<E> header;
+    private Node<E> trailer;
+    private int count;
+
+    private class Node<T> {
 
         public T element;
         public Node<T> next;
@@ -22,22 +29,46 @@ public class ListDoubleLinked<E> implements ListTAD<E> {
             next = null;
             prev = null;
         }
+
     }
-    private Node<E> header;
-    private Node<E> trailer;
-    private int count;
 
     public ListDoubleLinked() {
-        header = new Node<E>(null);
-        trailer = new Node<E>(null);
+        this.header = new Node<>(null);
+        this.trailer = new Node<>(null);
         header.next = trailer;
         trailer.prev = header;
-        count = 0;
+        this.count = 0;
+    }
+ @Override
+    public Iterator<E> iterator() {
+        return (new Iterator<E>() {
+            private Node<E> current = header.next;
+
+            @Override
+            public boolean hasNext() {
+                return current != trailer;
+            }
+
+            @Override
+            public E next() {
+                if (current == trailer) {
+                    throw new NoSuchElementException();
+                }
+                E item = current.element;
+                current = current.next;
+                return item;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        });
     }
 
     @Override
     public void add(E element) {
-        Node<E> novoNodo = new Node(element);
+        Node<E> novoNodo = new Node<>(element);
         novoNodo.next = trailer;
         novoNodo.prev = trailer.prev;
         trailer.prev.next = novoNodo;
@@ -56,17 +87,17 @@ public class ListDoubleLinked<E> implements ListTAD<E> {
             novoNodo.prev = trailer.prev;
             trailer.prev.next = novoNodo;
             trailer.prev = novoNodo;
-            count++;
         } else {
             Node<E> aux = null;
-            if (index < count / 2) {
+            if (index == count / 2) {
                 aux = header.next;
                 for (int i = 0; i < index; i++) {
                     aux = aux.next;
                 }
+
             } else {
                 aux = trailer.prev;
-                for (int i = count - 1; i > index; i--) {
+                for (int i = count-1; i > index; i--) {
                     aux = aux.prev;
                 }
             }
@@ -75,26 +106,35 @@ public class ListDoubleLinked<E> implements ListTAD<E> {
             aux.prev.next = novoNodo;
             aux.prev = novoNodo;
         }
+        
+        count++;
     }
 
     @Override
     public E get(int index) {
+        E elem = null;
         if (index < 0 || index > count) {
             throw new IndexOutOfBoundsException();
         }
-        Node<E> aux = null;
-        if (index < count / 2) {
-            aux = header.next;
-            for (int i = 0; i < index; i++) {
-                aux = aux.next;
-            }
+        if (index == count) {
+            elem = header.next.element;
         } else {
-            aux = trailer.prev;
-            for (int i = count - 1; i > index; i--) {
-                aux = aux.prev;
+            Node<E> aux = null;
+            if (index == count / 2) {
+                aux = header.next;
+                for (int i = 0; i < index; i++) {
+                    aux = aux.next;
+                }
+
+            } else {
+                aux = trailer.prev;
+                for (int i = count; i > index; i--) {
+                    aux = aux.prev;
+                }
             }
+            elem = aux.element;
         }
-        return aux.element;
+        return elem;
     }
 
     @Override
@@ -122,16 +162,15 @@ public class ListDoubleLinked<E> implements ListTAD<E> {
 
     @Override
     public boolean remove(E element) {
-        Node<E> aux=header.next;
-        for(int i=0; i<count;i++){
-            if(aux.element.equals(element)){
-                aux.prev.next=aux.next;
-                aux.next.prev=aux.prev;
+        Node<E> aux = header.next;
+        for (int i = 0; i < count; i++) {
+            if (aux.element.equals(element)) {
+                aux.prev.next = aux.next;
+                aux.next.prev = aux.prev;
                 count--;
                 return true;
-            }
-            else{
-                aux=aux.next;
+            } else {
+                aux = aux.next;
             }
         }
         return false;
@@ -162,7 +201,7 @@ public class ListDoubleLinked<E> implements ListTAD<E> {
 
     @Override
     public boolean isEmpty() {
-      return (header.next==trailer);
+        return (header.next == trailer);
     }
 
     @Override
@@ -172,13 +211,12 @@ public class ListDoubleLinked<E> implements ListTAD<E> {
 
     @Override
     public boolean contains(E element) {
-         Node<E> aux=header.next;
-        for(int i=0; i<count;i++){
-            if(aux.element.equals(element)){
+        Node<E> aux = header.next;
+        for (int i = 0; i < count; i++) {
+            if (aux.element.equals(element)) {
                 return true;
-            }
-            else{
-                aux=aux.next;
+            } else {
+                aux = aux.next;
             }
         }
         return false;
@@ -196,14 +234,25 @@ public class ListDoubleLinked<E> implements ListTAD<E> {
             index++;
         }
         return -1;
-        
+
     }
 
     @Override
     public void clear() {
-        header.next=trailer;
-        trailer.prev= header;
-        count=0;
+        header.next = trailer;
+        trailer.prev = header;
+        count = 0;
     }
-
+    
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        Node<E> aux = header.next;
+        for (int i = 0; i < count; i++) {
+            s.append(aux.element.toString());
+            s.append("\n");
+            aux = aux.next;
+        }
+        return s.toString();
+    }
 }
